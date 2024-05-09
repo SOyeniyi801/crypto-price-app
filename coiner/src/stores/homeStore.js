4925cc83a9f4a3d8c2358e5dca4358e7f9e39c76
soyeniyi801 <naturalbeauty801@gmail.com>
2024-05-09 13:57:22 -0400

    completed app

--- coiner/src/stores/homeStore.js
+++ coiner/src/stores/homeStore.js
@@ -6,6 +6,8 @@ const homeStore = create((set) => ({
   coins: [],
   trending: [],
   query: "",
+  searching: false,
+  searched: false,
 
   setQuery: (e) => {
     set({ query: e.target.value });
@@ -13,6 +15,7 @@ const homeStore = create((set) => ({
   },
 
   searchCoins: debounce(async () => {
+    set({searching: true})
     const { query, trending } = homeStore.getState();
 
     if (query.length > 2) {
@@ -27,25 +30,31 @@ const homeStore = create((set) => ({
           id: coin.id,
         };
       });
-      set({ coins });
+      set({ coins, searching: false, searched: true});
     } else {
-      set({ coins: trending });
+      set({ coins: trending, searching: false, searched: false });
     }
   }, 500),
   fetchCoins: async () => {
-    const response = await axios.get(
-      "https://api.coingecko.com/api/v3/search/trending"
-    );
+    const [res, btcRes,] = await Promise.all ([
+      axios.get('https://api.coingecko.com/api/v3/search/trending'),
+      axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+    ])
+    
+    const btcPrice = btcRes.data.bitcoin.usd; 
+    console.log("Price",btcPrice)
 
-    const coins = response.data.coins.map((coin) => {
+    const coins = res.data.coins.map((coin) => {
       return {
         name: coin.item.name,
         image: coin.item.large,
         id: coin.item.id,
-        priceBtc: coin.item.price_btc,
+        priceBtc: (coin.item.price_btc).toFixed(10),
+        priceUsd: (coin.item.price_btc * btcPrice).toFixed(6)
       };
     });
 
+    console.log(coins)
     set({ coins, trending:coins });
   },
 }));
--- coiner/src/stores/showStore.js
+++ coiner/src/stores/showStore.js
@@ -2,9 +2,35 @@ import { create } from "zustand";
 import axios from "axios";
 
 const showStore = create((set) => ({
+  graphData: [],
+  data:{},
+
+  reset: () => {
+    set({graphData: [],data:{} })
+  },
   fetchData: async (id) => {
-   const res =  await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7`)
-   console.log(res)
+    const [graphRes, dataRes] = await Promise.all([
+        axios.get(
+        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=121`
+      ),
+      axios.get(
+        `https://api.coingecko.com/api/v3/coins/${id}?localization=false`
+      ),
+      
+    ]);
+
+    const graphData = graphRes.data.prices.map((price) => {
+      const [timestamp, p] = price;
+      const date = new Date(timestamp).toLocaleDateString("en-us");
+      return {
+        Date: date,
+        Price: p,
+      };
+    });
+    console.log(dataRes);
+
+    set({ graphData, data: dataRes.data  });
+    
   },
 }));
 
